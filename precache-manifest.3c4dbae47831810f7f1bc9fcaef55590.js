68a3275b6087f1fbe059360be11eab2f40a33786
Jason Yee <jasonyee1776@gmail.com>
2020-11-11 00:44:21 -0800

    Updates

---
+++ precache-manifest.3c4dbae47831810f7f1bc9fcaef55590.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a863245f13f48cf3bb5b",
+    "url": "/react-practice-state/static/css/main.c53c97e3.chunk.css"
+  },
+  {
+    "revision": "a863245f13f48cf3bb5b",
+    "url": "/react-practice-state/static/js/main.fa7aabea.chunk.js"
+  },
+  {
+    "revision": "83e0dfc919a83509e526",
+    "url": "/react-practice-state/static/js/runtime~main.e414d69e.js"
+  },
+  {
+    "revision": "64083028d8fb527e4272",
+    "url": "/react-practice-state/static/js/2.decfa85c.chunk.js"
+  },
+  {
+    "revision": "654c7962cc469e47baa54452d13f64a2",
+    "url": "/react-practice-state/index.html"
+  }
+];
