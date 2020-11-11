3229994ffb3190dd9d132b7b9bedd24e039edcc2
Jason Yee <jasonyee1776@gmail.com>
2020-11-11 15:57:41 -0800

    added star rating functionality

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -5,31 +5,42 @@ class StarRating extends Component {
 
   // Initialize a 'rating' state
   state = {
-    rating: ""
+    rating: 0
   }
-
-  starArray = [];
+ 
 
   // Write a function that returns 5 Star components
-  handleStars = () => {
-      for (let i=0;i<=5;i++) {
-        this.starArray.push(<Star />);
-        console.log(this.starArray)
-        return this.starArray;
+  renderStars = () => {
+      let starArray = [];
+      let maxStars = 5;
+
+      for (let i = 0; i < maxStars; i++) {
+        starArray.push(
+          <Star 
+            key={i}
+            updateRating={ () => this.updateRating(i + 1) }
+            isSelected={this.state.rating > i}
+          />
+        );
       }
-    
+      return starArray;
   }
 
   // Write an event handler that updates the rating state.
+  updateRating = (rating) => {
+    this.setState({ rating: rating})
+  }
+  
+
   // Pass the function to a Star component via props
 
 
   render() {
     return (
       <ul className="course--stars">
-        {/* Render the Star components */
-          this.handleStars
-        }
+        {/* Render the Star components */}
+        {this.renderStars()}
+        
       </ul>
     );
   }
